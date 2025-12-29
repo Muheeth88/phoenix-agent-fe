55388861bc906075d50f1ee6c550db8a6d83ca82
Muheeth <abdulmuheeth88@gmail.com>
2025-12-30 02:55:52 +0530

    Packages added

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,9 @@
 const constants = {
-  API_BASE_URL:
-    import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api",
+	API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
 };
 
 const queryKeys = {
-  USERS: "USERS",
+	USERS: "USERS",
 };
 
 export { constants, queryKeys };
